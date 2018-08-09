62bc3b12c08d04754e11b6cee7ddd28a3a4f4fb3
lsl <songlin.lin@yundata.com>
2018-08-10 07:59:07 +0800

    修改stack算法

--- 10/stack.go
+++ 10/stack.go
@@ -2,15 +2,53 @@ package main
 
 import "fmt"
 
-func stack_empty() {
+type Stack struct {
+	top int
+	S   []int
 }
 
-func stack_push() {
+func (s *Stack) stack_empty() bool {
+	if len(s.S) == 0 {
+		return true
+	} else {
+		return false
+	}
 }
 
-func stack_pop() {
+func (s *Stack) stack_push(v int) {
+	fmt.Println(s.S, len(s.S), cap(s.S))
+	if s.top < len(s.S)-1 {
+		s.top += 1
+		s.S[s.top] = v
+	} else {
+		s.S = append(s.S, v)
+		s.top += 1
+	}
+}
+
+func (s *Stack) stack_pop() int {
+	if s.top > 0 {
+		s.top--
+		return s.S[s.top+1]
+	}
+	return -1
+}
+
+func NewStack() *Stack {
+	return &Stack{-1, make([]int, 2)}
 }
 
 func main() {
-	fmt.Println("vim-go")
+	s := NewStack()
+	s.stack_push(1)
+	s.stack_push(2)
+	s.stack_push(3)
+	s.stack_push(4)
+	s.stack_push(5)
+	s.stack_push(5)
+	s.stack_push(5)
+	s.stack_push(5)
+	s.stack_push(5)
+	s.stack_push(5)
+	fmt.Println(s.S)
 }
